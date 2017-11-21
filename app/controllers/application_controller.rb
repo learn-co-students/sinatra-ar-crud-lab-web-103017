83603df25d9bc568cc5aca8fde821bf5e02697b0
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-21 21:03:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,44 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  #create
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    new_post = Post.create(name: params[:name],  content: params[:content])
+    redirect "/posts"
+  end
+  #read
+  get '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  #Update
+  get '/posts/:id/edit' do
+    @id = params[:id]
+    @post = Post.find_by(id: @id)
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    upost = Post.find_by(id: params[:id])
+    upost.update(name: params[:name], content: params[:content])
+    id = params[:id]
+    redirect "/posts/#{id}"
+  end
+
+  #delete
+  delete '/posts/:id/delete' do
+    post = Post.find_by(id: params[:id])
+    @post = post.name
+    post.destroy
+    erb :delete
   end
 end
---
+++ db/migrate/20171121190510_change_posts.rb
@@ -0,0 +1,4 @@
+class ChangePosts < ActiveRecord::Migration
+  def change
+  end
+end
