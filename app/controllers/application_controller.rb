d08f4abb51f836995c4a4c36addbcc6ac3673466
github username <github email address>
2017-11-26 15:32:28 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  # get '/' do
+  #   redirect '/posts'
+  # end
+
+  get '/posts' do
+    @all_posts = Post.all
+    erb :index
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @new_post = Post.create(name: params[:name], content: params[:content])
+    redirect '/posts'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    @new_post = Post.find_by(id: params[:id])
+    @new_post.name = params[:name]
+    @new_post.content = params[:content]
+    @new_post.save
+    redirect "/posts/#{@new_post.id}"
+  end
+
+  get '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    Post.delete(@post)
+    erb :delete
+  end
+
 end
